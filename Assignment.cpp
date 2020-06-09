b7c07e464e12e331e816a1063273776bce5cce83
Yash Turkar <yashturkar@gmail.com>
2020-06-09 20:17:06 +0530

    Add files via upload

---
+++ Assignment.cpp
@@ -0,0 +1,36 @@
+// Convert this program to C++
+
+// change to C++ io
+
+// change to one line comments
+
+// change defines of constants to const
+
+// change array to vector<>
+
+// inline any short function
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 40;
+
+inline void sum(int *p,int n,vector<int> d) {*p = 0; for(int i=0,i<n;i++) *p = *p + d[i]}
+
+int main()
+{
+    int accum = 0;
+    vector<int> data;
+
+    for(int i = 0,i < N, ++i)
+    data[i] = i
+
+    sum(&accum, data.size(), data)
+
+    cout << "Sum is \n" << accum ;
+
+    return 0;
+
+}
