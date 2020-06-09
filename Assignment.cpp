4685190529cda374a929e341eb88cc1af185ee6e
Yash Turkar <yashturkar@gmail.com>
2020-06-09 20:26:13 +0530

    Add files via upload

--- Assignment.cpp
+++ Assignment.cpp
@@ -17,19 +17,20 @@ using namespace std;
 
 const int N = 40;
 
-inline void sum(int *p,int n,vector<int> d) {*p = 0; for(int i=0,i<n;i++) *p = *p + d[i]}
+inline void sum(int *p,int n,vector<int> d) {*p = 0; for(int i=0;i<n;i++){ *p = *p + d[i];}}
 
 int main()
 {
     int accum = 0;
     vector<int> data;
 
-    for(int i = 0,i < N, ++i)
-    data[i] = i
+    for(int i = 0;i < N; ++i)
+    
+    data.push_back(i);
 
-    sum(&accum, data.size(), data)
+    sum(&accum, data.size(), data);
 
-    cout << "Sum is \n" << accum ;
+    cout << "Sum is \n" << accum << endl;
 
     return 0;
 
